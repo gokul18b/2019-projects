0678048984a449c5240d511b87a0fc440446bc73
Gokul <gokul18b@gmail.com>
2020-02-19 20:06:38 +0530

    granite verified

--- java/granite/granite.sql
+++ java/granite/granite.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 14, 2020 at 05:59 AM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.11
+-- Generation Time: Feb 19, 2020 at 03:35 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,25 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `granite`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `username` text NOT NULL,
-  `password` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -63,32 +44,28 @@ CREATE TABLE `customer` (
 INSERT INTO `customer` (`id`, `name`, `mobile`, `address`, `gender`, `email`) VALUES
 (1, 'name', 'mobile', 'address', 'gender', 'email'),
 (2, 'kjh', 'jk', 'hkj', 'Male', 'kjhk'),
-(3, 'kjh', 'jk', 'hkj', 'Male', 'kjhk');
+(3, 'kjh', 'jk', 'hkj', 'Male', 'kjhk'),
+(4, 'undefined', 'undefined', 'undefined', 'undefined', 'undefined'),
+(5, '89', '87', '979', 'Male', '979');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employee`
+-- Table structure for table `manager`
 --
 
-CREATE TABLE `employee` (
+CREATE TABLE `manager` (
   `id` int(11) NOT NULL,
-  `name` text NOT NULL,
-  `mobile` text NOT NULL,
-  `address` text NOT NULL,
-  `gender` text NOT NULL,
-  `salary` int(11) NOT NULL,
-  `age` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `username` text NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `employee`
+-- Dumping data for table `manager`
 --
 
-INSERT INTO `employee` (`id`, `name`, `mobile`, `address`, `gender`, `salary`, `age`) VALUES
-(1, 'name', 'mobile', 'address', 'gender', 1, 1),
-(2, '8', '89', '89', 'Male', 89, 89),
-(3, 'sd', 'sdf', '23', 'Male', 23, 3);
+INSERT INTO `manager` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -99,7 +76,8 @@ INSERT INTO `employee` (`id`, `name`, `mobile`, `address`, `gender`, `salary`, `
 CREATE TABLE `product` (
   `id` int(11) NOT NULL,
   `company` text NOT NULL,
-  `model` text NOT NULL,
+  `product` text NOT NULL,
+  `weight` text NOT NULL,
   `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -107,9 +85,10 @@ CREATE TABLE `product` (
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id`, `company`, `model`, `price`) VALUES
-(3, 'MOTO', 'G3', 12000),
-(4, 'OPPO', 'F11 PRO', 14000);
+INSERT INTO `product` (`id`, `company`, `product`, `weight`, `price`) VALUES
+(3, 'MOTO', 'G3', '', 12000),
+(4, 'OPPO', 'F11 PRO', '', 14000),
+(5, '980k', 'klj', '08', 98);
 
 -- --------------------------------------------------------
 
@@ -121,18 +100,21 @@ CREATE TABLE `purchase` (
   `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
+  `price` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `purchase`
 --
 
-INSERT INTO `purchase` (`id`, `product_id`, `quantity`, `date`) VALUES
-(5, 3, 5, '2020-02-11 11:33:33'),
-(6, 3, 2, '2020-02-11 11:33:37'),
-(7, 4, 5, '2020-02-11 11:49:29'),
-(8, 4, 2, '2020-02-11 12:07:37');
+INSERT INTO `purchase` (`id`, `product_id`, `quantity`, `price`, `date`) VALUES
+(5, 3, 5, 0, '2020-02-11 11:33:33'),
+(6, 3, 2, 0, '2020-02-11 11:33:37'),
+(7, 4, 5, 0, '2020-02-11 11:49:29'),
+(8, 4, 2, 0, '2020-02-11 12:07:37'),
+(9, 3, 23, 34, '2020-02-19 10:30:50'),
+(10, 5, 4, 2, '2020-02-19 10:53:21');
 
 -- --------------------------------------------------------
 
@@ -144,27 +126,25 @@ CREATE TABLE `sales` (
   `id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `product_id` text NOT NULL,
-  `quantity` int(11) NOT NULL
+  `quantity` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `sdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `sales`
 --
 
-INSERT INTO `sales` (`id`, `customer_id`, `product_id`, `quantity`) VALUES
-(4, 2, '3', 1),
-(5, 2, '3', 1),
-(6, 1, '4', 1);
+INSERT INTO `sales` (`id`, `customer_id`, `product_id`, `quantity`, `price`, `sdate`) VALUES
+(4, 2, '3', 1, 0, '2020-02-19 11:05:25'),
+(5, 2, '3', 1, 0, '2020-02-19 11:05:25'),
+(6, 1, '4', 1, 0, '2020-02-19 11:05:25'),
+(7, 1, '3', 23, 345, '2020-02-19 11:05:25'),
+(8, 1, '5', 2, 345, '2020-02-19 11:05:25');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `customer`
@@ -173,9 +153,9 @@ ALTER TABLE `customer`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `employee`
+-- Indexes for table `manager`
 --
-ALTER TABLE `employee`
+ALTER TABLE `manager`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -201,40 +181,34 @@ ALTER TABLE `sales`
 --
 
 --
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `employee`
+-- AUTO_INCREMENT for table `manager`
 --
-ALTER TABLE `employee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `manager`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `purchase`
 --
 ALTER TABLE `purchase`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
