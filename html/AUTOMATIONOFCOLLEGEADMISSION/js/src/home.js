7174c3c2491f59046e3f131d8d1d4ed957bef394
Gokul <gokul18b@gmail.com>
2020-02-19 19:52:24 +0530

    upto verify admission

--- html/AUTOMATIONOFCOLLEGEADMISSION/js/src/home.js
+++ html/AUTOMATIONOFCOLLEGEADMISSION/js/src/home.js
@@ -1,6 +1,7 @@
 var final_id;
 
 $(document).ready(function(){
+	
 	getDepartment();
 	getClass();
 	getDepartmentClass();
@@ -46,6 +47,25 @@ function studentregistration(){
 			});
 
 }
+
+function login(){
+	var username = $("#username").val();
+	var password = $("#password").val();
+	
+	$.ajax({
+			type:"GET",
+			url:"http://localhost:8080/api/login/"+username+"/"+password,
+			success: function(data) {
+				
+				if(data){
+					window.location.href="/admission/student-registration.html"
+				}else{
+					alert('Invalid username or password')
+				}
+			}
+			});
+}
+
 
 function getDepartment(){
 	$.ajax({
